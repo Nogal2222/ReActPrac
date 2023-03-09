eea77c093af233988fdbeaa38f78b4291bb073b0
njh3321 <njh3321@naver.com>
2023-03-09 17:08:45 +0900

    feature : 컴포넌트 연습 중

---
+++ chap1/basic/src/AppJSX.jsx
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function AppJSX() {
+  const name = "junho";
+  const list = ['우유', '딸기', '바나나']
+  return (
+    <>
+      <h1 className="orange">
+        Hello!
+      </h1>
+      <p className="orange">{name}</p>
+      <h1> {`Hello! ${name}`}</h1>
+      <ul>
+        {list.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default AppJSX;
